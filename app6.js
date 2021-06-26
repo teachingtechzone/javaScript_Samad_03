57dc15c95ba8a0821a46bcf54d7688c486c12291
hasnain <teachingtechzone@gmail.com>
2021-06-26 18:23:39 +0500

    added 6th class

---
+++ app6.js
@@ -0,0 +1,63 @@
+
+
+// var qty = 2;
+// var color = "red";
+
+// checking condition
+// if ( qty >= 80 ) { // false
+//     console.log("more than enogh stock")
+// } 
+// else if( qty >= 70 ){ // false
+//     console.log("enogh stock")
+// }
+// else if (qty >= 60){ // false
+//     console.log("stock is reaching to ending")
+// }
+// else{ 
+//     console.log("you should go and buy more items")
+// }
+
+
+
+// and operator
+// if ( qty >= 80 && color == "blue") { 
+//     console.log("more than enogh stock")
+// } 
+// else if( qty >= 70 && color == "blue" ){ 
+//     console.log("enogh stock")
+// }
+// else if (qty >= 60 && color == "blue"){ 
+//     console.log("stock is reaching to ending")
+// }
+// else{ 
+//     console.log("you should go and buy more items")
+// }
+
+
+
+
+// or operator
+// if ( qty >= 80 || color == "blue") { 
+//     console.log("more than enogh stock")
+// } 
+// else if( qty >= 70 || color == "blue" ){ 
+//     console.log("enogh stock")
+// }
+// else if (qty >= 60 || color == "blue"){ 
+//     console.log("stock is reaching to ending")
+// }
+// else{ 
+//     console.log("you should go and buy more items")
+// }
+
+
+// not operator
+// if(color != "blue"){ // 
+//     console.log("color is not blue");
+// }
+// else{
+//     console.log("color is blue")
+// }
+// var myname = "samad";
+// var mynumber = 1;
+// console.log(mynumber)
